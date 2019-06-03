6c1b1ca07b1fffdcc530787c0dd2668ad8294832
Oudom <oudomtata@gmail.com>
2019-06-03 15:23:08 -0500

    one step further

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,28 @@
 $( document ).ready(function() {
 
   var username = localStorage.getItem("username");
+  username = username.toUpperCase();
 
   var namediv = document.createTextNode(username + "'s To Do list");
   $("#header-title").append(namediv);
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyAAYOYa6uiZjZr47mRQ0JmhCJML4geiTkk",
-    authDomain: "class-project-328c0.firebaseapp.com",
-    databaseURL: "https://class-project-328c0.firebaseio.com",
-    projectId: "class-project-328c0",
-    storageBucket: "class-project-328c0.appspot.com",
-    messagingSenderId: "201869728141",
-    appId: "1:201869728141:web:ecb3fff10ff3ef2d"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
+  // var firebaseConfig = {
+  //   apiKey: "AIzaSyAAYOYa6uiZjZr47mRQ0JmhCJML4geiTkk",
+  //   authDomain: "class-project-328c0.firebaseapp.com",
+  //   databaseURL: "https://class-project-328c0.firebaseio.com",
+  //   projectId: "class-project-328c0",
+  //   storageBucket: "class-project-328c0.appspot.com",
+  //   messagingSenderId: "201869728141",
+  //   appId: "1:201869728141:web:ecb3fff10ff3ef2d"
+  // };
+  // // Initialize Firebase
+  // firebase.initializeApp(firebaseConfig);
+  // var database = firebase.database();
 
   var activities = {
     active :{
      phrase: ["Go play Soccer", "Go Jogging", "Pratice Yoga", "Learn how to ride a Skateboard", "Get up and Dance", "Hit the Gym"],
-     things: ["Soccer", "Jogging", "Yoga", "Skateboard tutorial", "Dance move for beginner", "Best Gym workout"],
+     things: ["Soccer", "Jogging", "Yoga", "Skateboard tutorial", "Dancing", "Best Gym workout"],
     },
     food :{
     phrase: ["Eat fancy pizza", "Eat a popular hamburger", "Drink some good beer", "Try noodles", "Good steak"],
@@ -29,7 +30,7 @@ $( document ).ready(function() {
     },
     art :{
     phrase: ["Play a song on the guitar", "Try painting", "Do Arts and Crafts", "Write poetry", "Do ceramic"],
-    things: ["beginner guitar tutorial", "how to paint", "Arts and Craft tutorial", "poetry", "ceramic tutorial"],
+    things: ["beginner guitar tutorial", "how to do oil painting", "Arts and Craft tutorial", "poetry", "ceramic tutorial"],
     },
     games :{
     phrase: ["Play someone in chess", "Play pictionary", "Play someone in checkers", "Play scrabble", "Play yatze", "Learn Mahjong"],
@@ -134,11 +135,11 @@ $( document ).ready(function() {
     var videosnum = Math.floor(Math.random()*activities.videos.phrase.length);
     var getvideos = activities.videos;
 
-
-    displayvideo(getactive.things[activenum],getactive,activenum);
+    displaygif(getactive,activenum);
+    // displayvideo(getactive.things[activenum],getactive,activenum);
     // displayvideo(getfood.things[foodnum],getfood, foodnum);
-    // displayvideo(getart.things[artnum], getart, artnum);
-    // displayvideo(getgames.things[gamesnum], getgames, gamesnum);
+    displayvideo(getart.things[artnum], getart, artnum);
+    displayvideo(getgames.things[gamesnum], getgames, gamesnum);
     // displayvideo(getvideos.things[videosnum], getvideos, videosnum);
 
 
@@ -147,23 +148,92 @@ $( document ).ready(function() {
   randomize();
 
   
-function displaymovie(title){
+// function displaymovie(title, num){
 
-  console.log('here')
-  var movie = $(this).attr("data-name");
-  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+//   console.log('here')
+//   var movie = title.things[num];
+//   var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 
-  // Creates AJAX call for the specific movie button being clicked
+//   // Creates AJAX call for the specific movie button being clicked
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   })
+//   .then(function(response) {
+//     var li = document.createElement("li");
+//     var inputValue = title.phrase[num];
+
+//     var t = document.createTextNode(inputValue );
+//     t.ClassName= "center";
+//     li.appendChild(t);
+//     document.getElementById("myUL").appendChild(li);      
+//     //document.getElementById("myInput").value = "";
+  
+//     var span = document.createElement("SPAN");
+//     var txt = document.createTextNode("Done");
+//     span.className = "close";
+//     span.appendChild(txt);
+    
+//     li.appendChild(span);
+  
+//     for (i = 0; i < close.length; i++) {
+//       close[i].onclick = function() {
+//         var div = this.parentElement;
+//         div.style.display = "none";
+//       }
+//     }
+
+//     var movdiv = $("<")
+  
+//   });
+
+
+//   }
+function displaygif(gif, num){
+  // Grabbing and storing the data-animal property value from the button
+  //var gif = $(this).attr("data-name");
+  
+  // Constructing a queryURL using the animal name
+  console.log(gif.things[num])
+
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+  gif.things[num] + "&api_key=wD26TG3fyEOmSPWd6KyRYyAHXYQGsUQ9&limit=5";
+  
+  // Performing an AJAX request with the queryURL
   $.ajax({
     url: queryURL,
-    method: "GET"
+    method: "GET",
   })
-  .then(function(response) {
-    console.log(queryURL);
-  console.log(response);
-  $(".movies-view").text(JSON.stringify(response));
+// After data comes back from the request
+.then(function(response) {
+  var li = document.createElement("li");
+    var inputValue = gif.phrase[num];
+
+    var t = document.createTextNode(inputValue );
+    t.ClassName= "center";
+    li.appendChild(t);
+    document.getElementById("myUL").appendChild(li);      
+    //document.getElementById("myInput").value = "";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("Done");
+    span.className = "close";
+    span.appendChild(txt);
+    
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+    console.log(response)
+    var animalImage = $("<img>");
+    animalImage.attr("src", response.data[0].images.fixed_height.url);
+    animalImage.addClass("gif center");
+    li.append(animalImage[0]);
     });
-}
 
-
+  }
 });
